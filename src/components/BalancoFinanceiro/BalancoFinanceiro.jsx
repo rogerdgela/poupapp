32bf4f805807bc573ac86607a7e54de10bf96dcc
DESKTOP-EF0P7ET\Motors BR Roger <dgelask8@gmail.com>
2025-04-08 17:11:51 -0300

    Instalação do Chart.js

--- src/components/BalancoFinanceiro/BalancoFinanceiro.jsx
+++ src/components/BalancoFinanceiro/BalancoFinanceiro.jsx
@@ -2,19 +2,35 @@ import Cartao from "../Cartao/Cartao";
 import CartaoCabecalho from "../Cartao/CartaoCabecalho/CartaoCabecalho";
 import CartaoCorpo from "../Cartao/CartaoCorpo/CartaoCorpo";
 import styled from "styled-components";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ArcElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Pie } from "react-chartjs-2";
 
-export const AreaChart = styled.div`
-  padding: var(--padding-xs);
-  width: 50%;
-  height: 50%;
-`;
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ArcElement,
+  Tooltip,
+  Legend,
+);
+
 
 const BalancoFinanceiro = () => {
   return (
     <Cartao>
       <CartaoCabecalho>Gastos por categoria</CartaoCabecalho>
       <CartaoCorpo>
-        <AreaChart></AreaChart>
+        <AreaChart>
+          <Pie data={data} />
+        </AreaChart>
       </CartaoCorpo>
     </Cartao>
   );
