db06ccfedcf7911ea8e2fe6706b8c4b68f3a5e9a
DESKTOP-EF0P7ET\Motors BR Roger <dgelask8@gmail.com>
2025-04-08 17:16:42 -0300

    Juntando os dados do gráfico

--- src/components/BalancoFinanceiro/BalancoFinanceiro.jsx
+++ src/components/BalancoFinanceiro/BalancoFinanceiro.jsx
@@ -24,6 +24,30 @@ ChartJS.register(
 
 
 const BalancoFinanceiro = () => {
+  const data = {
+    datasets: [
+      {
+        label: "Gastos por categoria",
+        data: [1200, 2000, 3000, 4000, 5000],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
     <Cartao>
       <CartaoCabecalho>Gastos por categoria</CartaoCabecalho>
